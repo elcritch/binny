68c7f3fcaa46e4a8f22a2294f3c538b52af8bd8b
Jaremy Creechley <creechley@gmail.com>
2025-12-05 23:35:43 -0700

    print location

--- sframe/stacktrace_override.nim
+++ sframe/stacktrace_override.nim
@@ -44,7 +44,7 @@ proc getBacktrace*(): string {.noinline, gcsafe, raises: [], tags: [].} =
     let symbols = symbolizeStackTrace(frames, gFuncSymbols)
     for i, sym in symbols:
       if i < frames.len:
-        result.add(&"{sym}\n")
+        result.add(&"{sym} (at 0x{frames[i].toHex()})\n")
 
 proc unhandledExceptionOverride(e: ref Exception) {.nimcall, tags: [], raises: [].} =
   {.cast(gcsafe), cast(tags: []).}:
